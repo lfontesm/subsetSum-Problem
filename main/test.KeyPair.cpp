5a0083bccffe6cf1570dcfcb794a7dbfb8c56827
Leonardo Fontes <moreirafontesleo@gmail.com>
2018-12-14 13:02:38 -0200

    Will explain any time soon

--- main/test.KeyPair.cpp
+++ main/test.KeyPair.cpp
@@ -1,5 +1,8 @@
+#include <set>
 #include "keyPair.hpp"
 #include "util.hpp"
+#include <algorithm>
+#include <functional>
 
 int main(int argc, char *argv[]){
     if (argc < 2){
@@ -29,9 +32,23 @@ int main(int argc, char *argv[]){
         // match.print_key();
         ++match;
     }
+    subset_map(match, T, m);
 
     KeyPair kp(a, b);
     KeyPair kp2(b, a);
+
+    // Declaring the type of Predicate that accepts 2 pairs and return a bool
+    typedef std::function<bool(pair<KeyPair, int>, pair<KeyPair, int>)> Comparator;
+ 
+	// Defining a lambda function to compare two pairs. It will compare two pairs using second field
+	Comparator compFunctor =
+        [](const pair<KeyPair, int> &elem1 , const pair<KeyPair, int> &elem2){
+            return elem1.second >= elem2.second;
+        };
+
+    // Declaring a set that will store the pairs using above comparision logic
+	set<pair<KeyPair, int>, Comparator> setOfWords(
+        m.begin(), m.end(), compFunctor);
 
     // if (kp < kp2)
     //     cout << "aaa\n";
@@ -40,10 +57,13 @@ int main(int argc, char *argv[]){
     cout << "kp: " << kp.fst << ";" << kp.snd << ". kp2: " << kp.fst << ";" << kp.snd << endl;
 
 
-    for (auto &p : m){
+    // for (auto &p : m){
+    //     cout << "Key: [" << p.first.fst << ";" << p.first.snd << "], Val: " << p.second << endl;
+    // }
+
+    for (auto &p : setOfWords)
         cout << "Key: [" << p.first.fst << ";" << p.first.snd << "], Val: " << p.second << endl;
-        // delete p.first;
-    }
+    
 
     return 0;
 }
--- src/keyPair.cpp
+++ src/keyPair.cpp
@@ -14,24 +14,21 @@ KeyPair::KeyPair(const Key &k1, const Key &k2) :
 Key subset_map(const Key &k, const vector<Key> &T, map<KeyPair, int, KeyPairComparator> &m){
     Key sum;
     for (int i = 0; i < N; ++i) {
-        if (bitP(k, i) == 0) continue;
-        for (int j{i}; j < N; j++){
-            if (bitP(k, i) && bitP(k, j)) {
-                // sum += T[i];
-                // KeyPair *kp = new KeyPair(T[i], T[j]);
-                KeyPair kp(T[i], T[j]);
+        if (bitP(k, i)) {
+            KeyPair kp(T[i], sum);
+            sum += T[i];
+            // KeyPair *kp = new KeyPair(T[i], T[j]);
 
-                map<KeyPair, int, KeyPairComparator>::iterator it;
+            map<KeyPair, int, KeyPairComparator>::iterator it;
 
-                it = m.find(kp);
-                if (it != m.end())
-                    it->second += 1;
-                else
-                    m.insert(make_pair(kp, 1));
+            it = m.find(kp);
+            if (it != m.end())
+                it->second += 1;
+            else
+                m.insert(make_pair(kp, 1));
 
-                // cout << "Key: [" << kp.fst << ";" << kp.snd << "]" << endl;
-                
-            }
+            // cout << "Key: [" << kp.fst << ";" << kp.snd << "]" << endl;
+            
         }
     }
     return sum;
